a8f74f794437d30c0a35142b721c7884d7b83c32
rosa <rosa.lee@dgmit.com>
2022-01-30 16:47:47 +0900

    Unsplash로부터 이미지 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import Photo from './components/Photo';
+import { ResponseGetImage } from './services/Unsplash/models';
+import UnsplashService from './services/Unsplash/UnsplashService';
 
 const App: React.FC = () => {
-  return <h1>App component</h1>;
+  const [photos, setPhotos] = useState<ResponseGetImage[]>([]);
+  const unsplashService = useMemo(() => new UnsplashService(), []);
+
+  useEffect(() => {
+    unsplashService.getImages().then((res) => {
+      setPhotos(res);
+    });
+  }, [unsplashService]);
+
+  return (
+    <>{photos.length && photos.map((p) => <Photo key={p.id} imageUrl={p.imageUrl} description={p.description} />)}</>
+  );
 };
 
 export default App;
